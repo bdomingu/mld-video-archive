09afff32f28a571a9b872744ecbb61a1f28b8e1c
bdomingu <beatrizdominguez14@gmail.com>
2023-04-13 13:18:29 -0600

    began working on nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,7 +3,16 @@ import styles from './Nav.module.css';
 export default function Nav() {
     return (
         <div className={styles.navContainer}>
-            <img className={styles.logo} src='/images/image8.png' />
+            <div className={styles.navContent}>
+                <div className={styles.logo}>
+                <img src='/images/image8.png' />
+                </div>
+                <div className={styles.text}>
+                <h2>Modern Life Dating</h2>
+                 <h3>Video Archive</h3>
+                </div>
+            </div>
+            
         </div>
     )
 }
