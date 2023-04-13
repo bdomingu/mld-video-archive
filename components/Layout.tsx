7da0e2a6d1b400e3d8ccdd1d7aafde4649020175
bdomingu <beatrizdominguez14@gmail.com>
2023-04-13 11:14:04 -0600

    first commit

---
+++ components/Layout.tsx
@@ -0,0 +1,18 @@
+import Nav from './Nav';
+import Footer from './Footer';
+
+type LayoutProps = {
+    children: React.ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({children}) => {
+    return (
+        <div>
+            <Nav/>
+            {children}
+            <Footer/>
+        </div>
+    )
+}
+
+export default Layout;
---
+++ components/Nav.tsx
@@ -0,0 +1,9 @@
+import styles from './Nav.module.css';
+
+export default function Nav() {
+    return (
+        <div className={styles.navContainer}>
+            <img className={styles.logo} src='/images/image8.png' />
+        </div>
+    )
+}
