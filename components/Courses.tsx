04dedeabafdee2f31d9c471fe9c8d8e192f0b5d1
Proschansky <samproschansky@gmail.com>
2023-05-29 11:59:54 -0400

    Updated the copy and added images.

--- components/Courses.tsx
+++ components/Courses.tsx
@@ -6,13 +6,10 @@ export default function Courses() {
         <>
         <div className={styles.container}>
             <div className={styles.image}>
-
             </div>
             <div className={styles.content}>
                 <h1>Dark Pass Harem</h1>
-                <p>Lorem ipsum dolor sit amet consectetur. Lorem facilisis iaculis 
-                pretium sagittis eget. Sollicitudin feugiat iaculis justo lacus bibendum.
-                 Non in eu est nec laoreet in dignissim</p>
+                <p>The course so powerful that Teachable had to ban it. Learn how to attract, manage and expand relationships with multiple women simultaneously.</p>
                  <div className={styles.learnMore}>
                 <Link href='/courseView'>
                 <button>Learn Now</button>
--- pages/courseHome.tsx
+++ pages/courseHome.tsx
@@ -1,23 +1,25 @@
-import Layout from '@/components/Layout';
-import styles from './courseHome.module.css';
-import Courses from '../components/Courses';
-import withAuth from '@/components/ProtectedRoute';
+import Layout from "@/components/Layout";
+import styles from "./courseHome.module.css";
+import Courses from "../components/Courses";
+import withAuth from "@/components/ProtectedRoute";
 
 const CourseHome = () => {
-    return (
-        <Layout>
-            <div className={styles.textContainer}>
-                <h1>My Courses</h1>
-                <p>Lorem ipsum dolor sit amet consectetur. Lorem facilisis iaculis 
-                 pretium sagittis eget. Sollicitudin feugiat iaculis justo lacus 
-                bibendum. Non in eu est nec laoreet in dignissim 
-                </p>
-            </div>
-            <div className={styles.courseContainer}>
-                <Courses/>
-            </div>
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <div className={styles.textContainer}>
+        <h1>My Courses</h1>
+        <p>
+          In this digital age, it's crucial to know how to present yourself
+          attractively online, decode and reciprocate non-verbal cues
+          accurately, and delve into the psychological intricacies of the women
+          you're interested in.
+        </p>
+      </div>
+      <div className={styles.courseContainer}>
+        <Courses />
+      </div>
+    </Layout>
+  );
+};
 
 export default withAuth(CourseHome);
