fc36350e0a7139f4d0529de907786b80435e95b6
bdomingu <beatrizdominguez14@gmail.com>
2023-08-29 17:57:46 -0600

    update for build

--- pages/api/database.ts
+++ pages/api/database.ts
@@ -4,14 +4,13 @@ const sequelize = new Sequelize({
   database: process.env.NEXT_PUBLIC_DATABASE,
   username: process.env.NEXT_PUBLIC_USER,
   password: process.env.NEXT_PUBLIC_PASSWORD,
-  host: process.env.NEXT_PUBLIC_HOST,
+  host: process.env.NEXT_PUBLIC_HOST === 'localhost' ? "http://localhost:3000" : `/cloudsql/${process.env.NEXT_PUBLIC_HOST}`,
   port: 3306, 
   dialect: 'mysql',
-//   dialectOptions: {
-//     socketPath: `/cloudsql/${process.env.NEXT_PUBLIC_HOST}`
-// },
+  dialectOptions: {
+    socketPath: `/cloudsql/${process.env.NEXT_PUBLIC_HOST}`
+},
 });
 
 export default sequelize;
 
-// === 'localhost' ? "http://localhost:3000" : `/cloudsql/${process.env.NEXT_PUBLIC_HOST}`,
